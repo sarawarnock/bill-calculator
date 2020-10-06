488c69798be505e6ac2147bff12f973d301721ff
sarawarnock <sara@sarawarnock.com>
2020-10-06 09:42:39 -0600

    calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,62 @@
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("The Bill Calculator");
+
+        //Save total number of diners
+        int numberOfPersons = 4;
+
+        //taxes
+        double taxPercent = 8;
+        double tipPercent = 15;
+
+        //split up by people and what they ordered
+        double personOneAppetizerCost = 9.99;
+        double personOneMainCost = 23.99;
+        double personOneDessertCost = 10.29;
+        double personOneDrinkCost = 8.50;
+
+        double personTwoAppetizerCost = 12.99;
+        double personTwoMainCost = 18.99;
+        double personTwoDessertCost = 9.99;
+        double personTwoDrinkCost = 4.25;
+
+        double personThreeAppetizerCost = 10.49;
+        double personThreeMainCost = 26.79;
+        double personThreeDessertCost = 10.29;
+        double personThreeDrinkCost = 3.75;
+
+        double personFourAppetizerCost = 13.79;
+        double personFourMainCost = 25.99;
+        double personFourDessertCost = 4.49;
+        double personFourDrinkCost = 7.50;
+
+        //using strings to declare data
+        String personOneName = "Adeline";
+        String personOneAppetizer = "Stuffed Ziti Fritta";
+        String personOneMain = "Shrimp Scampi";
+        String personOneDessert = "Sicilian Cheesecake";
+        String personOneDrink = "Raspberry Lemonade";
+
+        String personTwoName = "Brixton";
+        String personTwoAppetizer = "Lasagna Fritta";
+        String personTwoMain = "Fettuccine Alfredo";
+        String personTwoDessert = "Lemon Cream Cake";
+        String personTwoDrink = "Mango-Strawberry Iced Tea";
+
+        String personThreeName = "Cora";
+        String personThreeAppetizer = "Fried Mozzarella";
+        String personThreeMain = "Tour of Italy";
+        String personThreeDessert = "Zeppoli";
+        String personThreeDrink = "Fresh Brewed Iced Tea";
+
+        String personFourName = "Dean";
+        String personFourAppetizer = "Classic Shrimp Scampi Fritta";
+        String personFourMain = "Seafood Alfredo";
+        String personFourDessert = "Dolcini";
+        String personFourDrink = "Coke";
+
+        //using booleans
+        boolean leaveTip = true;
+        boolean splitEvenly = true;
     }
 }
--- src/TryItOut.java
+++ src/TryItOut.java
@@ -1,5 +1,28 @@
 public class TryItOut {
     public static void main(String[] args) {
-        
+        System.out.println("Trying out some stuff");
+
+        // Declare some variables (you can do this in two separate steps also)
+        int cupsOfCoffeeMonday = 4;
+        int cupsOfCoffeeTuesday = 3;
+        int cupsOfCoffeeWednesday = 6;
+        int cupsOfCoffeeThursday = 5;
+        int cupsOfCoffeeFriday = 4;
+
+        //Print one of them out
+        System.out.println(cupsOfCoffeeFriday);
+
+        //Long literals
+        long jeffBezosIncome = 11690000000L;
+        long usGdp = 20540000000000L;
+        long chinaGdp = 13610000000000L;
+        long turkeyGdp = 771400000000L;
+
+        //doubles and floats
+        double a = 0.2;
+        double b = 0.4;
+        double c = a * b;
+        System.out.println(c);
+        float temperature = 58.5F;
     }
 }
